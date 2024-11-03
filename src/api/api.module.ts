95e32ab352cf11899032a9d1033d135ac8d9259c
josephsb <jguku2002@gmail.com>
2024-11-03 18:27:18 -0500

    add vercel config

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@shared/guards/auth.guard';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule],
   controllers: [],
   providers: [
     {
