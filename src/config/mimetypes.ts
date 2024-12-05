2aac0b8b3460e93a3901749c7f6af0ca65d31223
josephsb <jguku2002@gmail.com>
2024-12-04 19:22:03 -0500

    fix errr

--- src/config/mimetypes.ts
+++ src/config/mimetypes.ts
@@ -1,7 +1,7 @@
 export const MIMETYPES = [
   { id: 1, name: 'image/gif', prefix: 'gif' },
   { id: 2, name: 'image/jpeg', prefix: 'jpeg' },
-  { id: 2, name: 'image/jpeg', prefix: 'jpg' },
+  { id: 2, name: 'image/jpg', prefix: 'jpg' },
   { id: 3, name: 'image/png', prefix: 'png' },
   { id: 4, name: 'image/webp', prefix: 'webp' },
 ];
